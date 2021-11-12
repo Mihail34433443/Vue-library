97532449a43151889152f513d8da93c4063cb6d7
Mihail34433443 <andich.mustafa@mail.ru>
2021-11-12 12:09:01 +0300

    develop

--- src/views/admin/order.vue
+++ src/views/admin/order.vue
@@ -37,7 +37,7 @@ export default {
   },
   async created() {
     await firebase
-      .firestore()
+      .firestore() 
       .collection("order")
       .doc(this.$route.query.order)
       .get()
